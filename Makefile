73c8d6b9c4c661642a19bf82c46fd89779a338b2
Leonard Glauch <lglauch@3-C-2.42heilbronn.de>
2024-06-10 14:44:52 +0200

    added out_handler

--- Makefile
+++ Makefile
@@ -3,10 +3,10 @@
 #                                                         :::      ::::::::    #
 #    Makefile                                           :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: rchavez <rchavez@student.42heilbronn.de    +#+  +:+       +#+         #
+#    By: lglauch <lglauch@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/04/29 17:50:33 by lglauch           #+#    #+#              #
-#    Updated: 2024/06/10 12:20:55 by rchavez          ###   ########.fr        #
+#    Updated: 2024/06/10 14:43:29 by lglauch          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,7 +14,7 @@ NAME = minishell
 CFLAGS	:= -Wextra -Wall -Werror
 SRCS	:= src/main.c src/globals.c src/utils.c src/ft_split_args.c src/tokenize.c src/signals.c src/path.c\
 libft/ft_split.c libft/ft_strlen.c libft/ft_strncmp.c src/free_tokens.c src/ft_error.c\
-src/utils2.c src/envp.c src/expand.c src/t_lexer.c src/t_file.c src/io_handler.c
+src/utils2.c src/envp.c src/expand.c src/t_lexer.c src/t_file.c src/io_handler.c src/out_handler.c
 OBJS	:= ${SRCS:src/%.c=src/%.o}
 
 all: $(NAME)
---
+++ src/out_handler.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   out_handler.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lglauch <lglauch@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/10 13:47:07 by lglauch           #+#    #+#             */
+/*   Updated: 2024/06/10 14:41:33 by lglauch          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/minishell.h"
+
+
+int	ft_get_fd(char *op, char *input)
+{
+	int	fd;
+
+	if (ft_strncmp(op, ">", 1) == 0)
+		fd = open(input, O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
+	else if (ft_strncmp(op, ">>", 2) == 0)
+		fd = open(input, O_WRONLY | O_CREAT | O_APPEND, PERMISSIONS);
+	return (fd);
+}
+
+void	out_to_file(char *quitter, char *op)
+{
+	char	*input;
+	int		i;
+	int		fd;
+
+	while (1)
+	{
+		input = readline("");
+		if (!input)
+		{
+			printf("Readline failed in out_function\n");
+			break ;
+		}
+		if (ft_strncmp(input, quitter, ft_strlen(quitter)) == 0)
+		{
+			free (input);
+			break ;
+		}
+		fd = ft_get_fd(op, input);
+		write(fd, input, ft_strlen(input));
+		write(fd, "\n", 1);
+		close(fd);
+		free(input);
+	}
+}
