6db759c5784889ae5ab1fef36153e8bea67f93b9
rodalcham <142218652+rodalcham@users.noreply.github.com>
2024-07-05 12:33:29 +0200

    fixed echo

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: leo <leo@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: rchavez <rchavez@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/10 16:04:20 by lglauch           #+#    #+#             */
-/*   Updated: 2024/07/05 11:37:30 by leo              ###   ########.fr       */
+/*   Updated: 2024/07/05 12:30:37 by rchavez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,145 +41,46 @@
 // 		return (0);
 // 	return (0);
 // }
-int	check_input(char *line, size_t i)
-{
-	size_t	j;
-
-	j = i + 2;
-	while (line[j] && !is_spc(line[j]))
-	{
-		if (line[j] != 'n')
-			return (1);
-		j++;
-	}
-	return (0);
-}
-
-void	remove_n_flag(char *line, int *index)
-{
-	size_t	i;
-	int		flag;
-	int		count;
-
-	flag = 0;
-	count = 0;
-	i = *index;
-	if (!line || !*line)
-		return ;
-	while (line[i] == '-' && line[i + 1] == 'n')
-	{
-		if (line[i + 2] == ' ' || line[i + 2] == 0 || line[i + 2] == 'n')
-		{
-			if (check_input(line, i) == 0)
-			{
-				i += 2;
-				flag = 1;
-				while (line[i] == 'n')
-				{
-					i++;
-					count++;
-				}
-				if (line[i] != ' ' && line[i] != 0)
-					flag = 0;
-				while (flag && line[i] == ' ')
-					i++;
-				if (!flag)
-					break ;
-			}
-			else
-				break ;
-		}
-		else
-			break ;
-	}
-	if (!flag)
-		*index = i - count;
-	else
-		*index = i;
-}
-
-char	*transform_correct(char *line, int n)
-{
-	int		i;
-	int		single;
-	int		doubl;
-	char	*new_str;
-	int		j;
-
-	i = 0;
-	single = 0;
-	doubl = 0;
-	j = 0;
-	new_str = malloc(sizeof(char) * ft_strlen(line) + 1);
-	if (!new_str)
-		return (NULL);
-	while (line[i])
-	{
-		while (is_spc(line[i]) && is_spc(line[i + 1]) && !single && !doubl)
-			i++;
-		if (n == 0)
-		{
-			remove_n_flag(line, &i);
-			n = 1;
-		}
-		new_str[j++] = line[i];
-		i++;
-	}
-	new_str[j] = 0;
-	return (new_str);
-}
 
 int	is_n_flag(char *str)
 {
-	if (str[0] != '-')
+	size_t	i;
+
+	i = 0;
+	if (!str || str[0] != '-')
 		return (0);
-	str++;
-	while (*str)
+	i++;
+	while (str[i])
 	{
-		if (*str != 'n')
+		if (str[i] != 'n')
 			return (0);
-		str++;
-	}
-	return (1);
-}
-
-char	*ft_combine_lexer(t_lexer *lexer)
-{
-	int		i;
-	char	*line;
-	char	*tmp;
-
-	i = 1;
-	line = ft_strdup("");
-	while (lexer->cmd[i])
-	{
-		tmp = ft_strjoin(line, lexer->cmd[i]);
-		free(line);
-		line = ft_strjoin(tmp, " ");
-		free(tmp);
 		i++;
 	}
-	return (line);
+	return (1);
 }
 
 int	echo_command(t_lexer *lexer)
 {
 	int		i;
 	int		newline;
-	char	*line;
+	char	**cmd;
 
 	i = 1;
 	newline = 1;
-	if (lexer->cmd[i] && is_n_flag(lexer->cmd[i]))
+	cmd = lexer->cmd;
+	if (cmd[i] && is_n_flag(cmd[i]))
 	{
 		newline = 0;
 		i++;
 	}
-	line = ft_combine_lexer(lexer);
-	line = transform_correct(line, newline);
-	if (line)
-		printf ("%s", line);
-	if (newline && printf("\n"))
-		return (0);
+	while (cmd[i])
+	{
+		printf("%s", cmd[i]);
+		if (cmd[i + 1])
+			printf(" ");
+		i++;
+	}
+	if (newline)
+		printf("\n");
 	return (0);
 }
